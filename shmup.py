34ce2e81fd2c1412497a2f1f6d845fde4a677b36
Johan Sulaiman <lai.s.johan@gmail.com>
2019-12-21 20:32:58 +0700

    sprite move updates

--- shmup.py
+++ shmup.py
@@ -16,11 +16,11 @@ BLUE = (0, 0, 255)
 game_folder = os.path.dirname(__file__)
 img_folder = os.path.join(game_folder, "img")
 
-class Player(pygame.sprite.Sprite):
+class Santa(pygame.sprite.Sprite):
     # sprite for the player
 	def __init__(self):
 		pygame.sprite.Sprite.__init__(self)
-		self.image = pygame.image.load(os.path.join(img_folder, "bigSanta.png")).convert()
+		self.image = pygame.image.load(os.path.join(img_folder, "cute santa.png")).convert()
 		self.image.set_colorkey(GREEN)
 		self.rect = self.image.get_rect()
 		self.rect.center = (WIDTH / 2, HEIGHT / 2)
@@ -39,23 +39,32 @@ class Player(pygame.sprite.Sprite):
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Cooldudes24 and Basherboy1 Game")
+pygame.display.set_caption("Cooldudes24 and Basherboy1 Shmup Game")
 clock = pygame.time.Clock()
 
 class Player(pygame.sprite.Sprite):
 	def __init__(self):
-		pygame.sprite.Sprite__init__(self)
+		pygame.sprite.Sprite.__init__(self)
 		self.image = pygame.Surface((50, 40))
-		self.image.fill(GREEN)
+		self.image.fill(BLACK)
 		self.rect = self.image.get_rect()
 		self.rect.centerx = WIDTH / 2
 		self.rect.bottom = HEIGHT - 10
 		self.speedx = 0
 
-		def update(self):
-			self.rectx += self.speedx
+	def update(self):
+		self.speedx = 0
+		keystate = pygame.key.get_pressed()
+		if keystate[pygame.K_LEFT]:
+			self.speedx = -5
+		if keystate[pygame.K_RIGHT]:
+			self.speedx = 5
+		self.rect.x += self.speedx
+		if self.rect.right > WIDTH
+		if self.rect.left
 
 all_sprites = pygame.sprite.Group()
+santa = Santa()
 player = Player()
 all_sprites.add(player)
 # Game loop
